c2e8b47e7da945808fe22b1d4af340fe1babb3ad
Carlos Santana <csantana23@gmail.com>
2015-09-08 17:41:03 -0400

    ionic promise pattern for wlCommonInit

--- blank/www/js/app.js
+++ blank/www/js/app.js
@@ -3,19 +3,88 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, MFPClientPromise) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+    MFPClientPromise.then(function(){WL.Logger.ctx({pkg: 'io.ionic'}).debug('mfp and ionic are ready, safe to use WL.* APIs');});
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
     }
   });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+}).factory('MFPClientPromise',function(){
+  return MFPClientDefer.promise;
 });
 
 var Messages = {
@@ -29,15 +98,18 @@ var wlInitOptions = {
     // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
 };
 
-// Called automatically after MFP framework initialization by WL.Client.init(wlInitOptions).
-function wlCommonInit(){
-	// Common initialization code goes here
+/* Setup a Promise to allow code to run in other places anytime after MFP CLient SDK is ready
+  Example: MFPClientPromise.then(function(){alert('mfp is ready, go ahead and use WL.* APIs')});
+*/
+var MFPClientDefer = angular.injector(['ng']).get('$q').defer();
+var wlCommonInit = MFPClientDefer.resolve;
+// wlCommonInit is called automatically after MFP framework initialization by WL.Client.init(wlInitOptions). It resolves the MFPClientPromise service
+MFPClientDefer.promise.then(function wlCommonInit(){
+  // Common initialization code goes here or use the service MFPClientPromise
   console.log('MobileFirst Client SDK Initilized');
-  angular.element(document).ready(function() {
-    mfpMagicPreviewSetup();
-    angular.bootstrap(document.body, ['starter']);
-  });
-}
+});
+
+
 
 function mfpMagicPreviewSetup(){
   //nothing to see here :-), just some magic to make ionic work with mfp preview, similar to ionic serve --lab
@@ -53,11 +125,3 @@ function mfpMagicPreviewSetup(){
 	} 
 }
 
-// Useful for ionic serve when MFP Client SDK is not present and wlCommonInit doesn't get called automatically
-var serveTimeout = 1500;
-window.setTimeout(function(){
-  if(typeof WL === 'undefined'){
-      console.log('MFP Client SDK timeout, running Web App');
-      angular.bootstrap(document.body, ['starter']);
-  }
-}, serveTimeout);
